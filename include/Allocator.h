3701497da6e2d3a4348e984678ff093a30f4f9dd
rakin406 <rakinhacker@gmail.com>
2023-12-14 20:19:18 +0600

    feat: Initialize m_ptr in constructor list

--- include/Allocator.h
+++ include/Allocator.h
@@ -5,13 +5,17 @@
 #ifndef HEAP_ALLOCATOR_ALLOCATOR_H
 #define HEAP_ALLOCATOR_ALLOCATOR_H
 
+#include <memory>
+
 class Allocator {
 public:
-    Allocator();
+    Allocator(std::size_t size);
 
     ~Allocator();
 
 private:
+    std::size_t m_size{};
+    std::unique_ptr<int[]> m_ptr{};
 };
 
 #endif //HEAP_ALLOCATOR_ALLOCATOR_H
--- src/Allocator.cpp
+++ src/Allocator.cpp
@@ -1,3 +1,11 @@
 //
 // Created by rakin on 12/14/2023.
 //
+
+#include "Allocator.h"
+
+#include <memory>
+
+Allocator::Allocator(std::size_t size)
+        : m_ptr{std::make_unique<int[]>(size)} {
+}
