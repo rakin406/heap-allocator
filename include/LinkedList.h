df80072603b233cc9c8db7f8cfe0ffd9f478c2c8
rakin406 <rakinhacker@gmail.com>
2023-12-14 23:29:52 +0600

    feat: Add head and tail members
    Add head and tail private pointers of Node type.

--- include/LinkedList.h
+++ include/LinkedList.h
@@ -5,14 +5,21 @@
 #ifndef HEAP_ALLOCATOR_LINKEDLIST_H
 #define HEAP_ALLOCATOR_LINKEDLIST_H
 
+/**
+ * @brief A custom implementation of linked list.
+ * @tparam T Type
+ */
+template<class T>
 class LinkedList {
 public:
 private:
-    template<class T>
     struct Node {
         T data{};
         Node *next{};
     };
+
+    Node *head{};
+    Node *tail{};
 };
 
 #endif //HEAP_ALLOCATOR_LINKEDLIST_H
