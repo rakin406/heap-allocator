14a6300494bac4ca20bfacb3f74c767573a15ff6
rakin406 <rakinhacker@gmail.com>
2023-12-14 23:20:47 +0600

    feat: Add Node structure
    Create Node struct with data and next fields.

--- include/LinkedList.h
+++ include/LinkedList.h
@@ -5,4 +5,14 @@
 #ifndef HEAP_ALLOCATOR_LINKEDLIST_H
 #define HEAP_ALLOCATOR_LINKEDLIST_H
 
+class LinkedList {
+public:
+private:
+    template<class T>
+    struct Node {
+        T data{};
+        Node *next{};
+    };
+};
+
 #endif //HEAP_ALLOCATOR_LINKEDLIST_H
