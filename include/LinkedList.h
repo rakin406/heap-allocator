5af0af01b19e6e2c5f3b498c660a13c2f7b40353
rakin406 <rakinhacker@gmail.com>
2023-12-15 11:48:05 +0600

    feat: Create clear method
    Create clear method which deletes all data from the list.

--- include/LinkedList.h
+++ include/LinkedList.h
@@ -32,30 +32,43 @@ template <class T> class LinkedList
      */
     const Node *append(T data)
     {
-        std::unique_ptr<Node> temp{};
-        temp->data = data;
-        temp->next = nullptr;
+        std::unique_ptr<Node> newNode{};
+        newNode->data = data;
+        newNode->next = nullptr;
 
         std::unique_ptr<Node> lastNode{nullptr};
 
         if (this->m_head == nullptr)
         {
-            this->m_head = temp;
+            this->m_head = newNode;
             lastNode = this->m_head;
         }
         else
         {
             // TODO: Choose a better name for this.
-            std::unique_ptr<Node> ptr{this->m_head};
-            while (ptr->next != nullptr)
+            std::unique_ptr<Node> temp{this->m_head};
+            while (temp->next != nullptr)
             {
-                ptr = ptr->next;
+                temp = temp->next;
             }
-            ptr->next = temp;
-            lastNode = ptr->next;
+            temp->next = newNode;
+            lastNode = temp->next;
         }
 
-        return lastNode;
+        return lastNode.get();
+    }
+
+    /**
+     * @brief Delete all data from the list.
+     */
+    void clear()
+    {
+        std::unique_ptr<Node> temp{this->m_head};
+        while (temp->next != nullptr)
+        {
+            this->m_head = this->m_head->next;
+            temp = temp->next;
+        }
     }
 
   private:
