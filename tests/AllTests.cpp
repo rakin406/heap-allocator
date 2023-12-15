b91b3470df4d8bd20baa4feb9bcb3fff260856ba
rakin406 <rakinhacker@gmail.com>
2023-12-15 17:04:43 +0600

    feat: Add AllTests file
    AllTests file runs all the tests.

---
+++ tests/AllTests.cpp
@@ -0,0 +1,12 @@
+//
+// Created by rakin on 12/15/2023.
+// This file runs all the tests.
+//
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
